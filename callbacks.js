66e6b9a1ff81e1da2868fd6587b69ef54b632ba3
VJ A <brite360@yahoo.com>
2015-09-19 18:55:22 -0700

    refactored functions to use the Each callback function

--- callbacks.js
+++ callbacks.js
@@ -1,84 +1,114 @@
+var arrayOfNumbers = [1,2,3,4,5];
+
+/* -------------------------------- */
+/* Each
+Write an 'each' function that takes a collection (array or object) and
+a callback function.  It is going to loop over every element in the collection and
+apply the callback function to it.
+When we define the callback function, we create a parameter that will be used to
+represent the current element in the array and we can then use that parameter in
+the callback function to define what we want to happen.
+*/
+
 function each(collection, callback) {
-    for (var i = 0; i < collection.length; i++) {
+	if (Array.isArray(collection)) {
+    for (var i=0; i<collection.length; i++) {
         callback(collection[i]);
     }
+	} else {
+		for (var prop in collection) {
+			callback(collection[prop]);
+		}
+	}
+
 }
-each([1,2,3,4,5], function(number){
+each(arrayOfNumbers, function(number){
     console.log(1 + number);
 }); //Logs out every number in the array plus 1
 
 
 /* -------------------------------- */
-/* Map */
+/* Map
+Write a 'map' function that takes a collection and a callback function.
+The callback function returns a new collection.
+*/
 
 var arrayOfNumbers = [1,2,3,4,5];
 
-var addFive = function(number) {
-	return number + 5;
-}
-var addThree = function(number) {
-	return number + 3;
-}
 var map = function(collection, callback) {
 	var arr = [];
 
-	for (var i=0; i<collection.length; i++) {
-		arr.push(callback(collection[i]));
-	}
-		return arr;
-}
+	each(collection, function(number){
+		arr.push(callback(number));
+	});
 
-var arrayPlusFive = map(arrayOfNumbers, addFive);
-console.log(arrayPlusFive); // [6,7,8,9,10]
-
-var arrayPlusThree = map(arrayOfNumbers, addThree);
-console.log(arrayPlusThree); // [4,5,6,7,8]
-
-/* -------------------------------- */
-/* Filter */
-
-var numbers = [1,2,-4,6,-12,24,2,-9];
-var getPositive = function(number) {
-	return number > 0;
-}
-var filter = function(collection, callback) {
-	var arr = [];
-	console.log(collection);
-	for (var i=0; i<collection.length; i++) {
-		console.log('callback(collection[i]) = ' + callback(collection[i]) + ", collection[i] = " + collection[i] );
-		if (callback(collection[i])===true) {
-			arr.push(collection[i]);
-		}
-	}
 	return arr;
 }
 
-var positiveNumbers = filter(numbers, getPositive); // [1,2,6,24,2]
-console.log(positiveNumbers)
+var arrayPlusThree = map(arrayOfNumbers, function(num){
+	return num + 3;
+});
+console.log(arrayPlusThree); // [4,5,6,7,8]
+
+var arrayPlusFive = map(arrayOfNumbers, function(num){
+	return num + 5;
+});
+console.log(arrayPlusFive); // [6,7,8,9,10]
+
 
 /* -------------------------------- */
-/* Reduced Number */
+/* Filter
+Write a 'filter' function that takes a collection and a callback function.
+The callback function returns a new collection and tests to see if the element
+belongs in the new collection.
+*/
 
-var addEmUp = function(sum, number) {
-	return sum + number;
-};
-var multiEmUp = function(multi, number, count) {
-	if (multi === 0 && count === 0) {
-		multi = 1;
-	}
-	return multi * number;
-};
+var arrayOfNumbers = [1,2,-4,6,-12,24,2,-9];
+
+var filter = function(collection, callback) {
+	var arr = [];
+
+	each(collection, function(number){
+		if (callback(number) === true) {
+			arr.push(number);
+		}
+	});
+
+	return arr;
+}
+
+var positiveNumbers = filter(arrayOfNumbers, function(num){
+	return num > 0;
+}); // [1,2,6,24,2]
+console.log(positiveNumbers);
+
+/* -------------------------------- */
+/* Reduced Number
+Write a 'reduce' function that takes a collection and a callback function.
+It will return a reduced version of all the numbers in a given collection.
+*/
+
+var arrayOfNumbers = [2,5,3,6,5];
+
 var reduce = function(collection, callback) {
-	var total=0;
-	for (var i=0;i<collection.length;i++) {
-		// console.log(total);
-		total = callback(total, collection[i] ,i);
-	}
+	var total;
+
+	each(collection, function(element){
+		if (total === undefined) {
+			total = element;
+		} else {
+			total = callback(total, element);
+		}
+	});
 	return total;
 };
 
-var reducedNumber = reduce([2,5,3,6,5], addEmUp); // 21
+var reducedNumber = reduce(arrayOfNumbers, function(sum, number){
+	return sum + number;
+}); // 21
 console.log(reducedNumber);
 
-var reducedNumber = reduce([2,4,7,2], multiEmUp); // 112
+var reducedNumber = reduce(arrayOfNumbers, function(multi, number){
+	return multi * number;
+}); // 900
 console.log(reducedNumber);
